4cefab8990a8e5315e31a4ccf67556d27722bd45
Sasha Zhuravlev <paperairplane89@gmail.com>
2015-06-18 19:29:56 +0300

    docs

---
+++ docs-assets/js/demos.js
@@ -0,0 +1,264 @@
+$("#demo .sollist").sollist({
+    profiles: {
+        facebook: 'http://facebook.com/zuck'
+        ,twitter: 'https://twitter.com/twitter'
+        ,deviantart:'http://mikymeg.deviantart.com/'
+        ,linkedin:'https://www.linkedin.com'
+        ,pinterest: 'https://www.pinterest.com/pin/475270566903389502/'
+        ,dribbble: 'https://dribbble.com/LeeGargano'
+        ,instagram: 'https://instagram.com/polinametelkova'
+    }
+    ,showTooltips: true
+    ,hoverEffect:'rise'
+    ,pixelsBetweenItems: 10
+    ,theme: 'mikymeg-color'
+    ,size: 32
+    ,tooltipsterOptions: {
+        theme: "sl-tooltip-black"
+    }
+    ,tooltips:{
+        'facebook': 'Zuck'
+        ,'deviantart': 'icons author page'
+        ,'instagram': 'pretty girl'
+    }
+});
+
+$("#themes .theme.lee-gargano-circle-color .sollist").sollist({
+    profiles: {
+        facebook: 'http://facebook.com/zuck'
+        ,twitter: 'https://twitter.com/twitter'
+        ,dribbble: 'https://dribbble.com/LeeGargano'
+    }
+    ,showTooltips: false
+    ,hoverEffect:''
+    ,pixelsBetweenItems: 20
+    ,theme: 'lee-gargano-circle-color'
+    ,size: 64
+    ,itemCss: {'padding-bottom': 0}
+});
+
+$("#themes .theme.lee-gargano-square-color .sollist").sollist({
+    profiles: {
+        instagram: 'https://instagram.com/polinametelkova'
+        ,deviantart:'http://mikymeg.deviantart.com/'
+        ,pinterest: 'https://www.pinterest.com/pin/475270566903389502/'
+    }
+    ,showTooltips: false
+    ,hoverEffect:''
+    ,pixelsBetweenItems: 20
+    ,theme: 'lee-gargano-square-color'
+    ,size: 64
+    ,itemCss: {'padding-bottom': 0}
+});
+
+$("#themes .theme.lee-gargano-circle-white .sollist").sollist({
+    profiles: {
+        instagram: 'https://instagram.com/polinametelkova'
+        ,dribbble: 'https://dribbble.com/LeeGargano'
+        ,pinterest: 'https://www.pinterest.com/pin/475270566903389502/'
+    }
+    ,showTooltips: false
+    ,hoverEffect:''
+    ,pixelsBetweenItems: 20
+    ,theme: 'lee-gargano-circle-white'
+    ,size: 64
+    ,itemCss: {'padding-bottom': 0}
+});
+
+$("#themes .theme.lee-gargano-square-white .sollist").sollist({
+    profiles: {
+        instagram: 'https://instagram.com/polinametelkova'
+        ,dribbble: 'https://dribbble.com/LeeGargano'
+        ,pinterest: 'https://www.pinterest.com/pin/475270566903389502/'
+    }
+    ,showTooltips: false
+    ,hoverEffect:''
+    ,pixelsBetweenItems: 20
+    ,theme: 'lee-gargano-square-white'
+    ,size: 64
+    ,itemCss: {'padding-bottom': 0}
+});
+
+$("#themes .theme.mikymeg-color .sollist").sollist({
+    profiles: {
+        tumblr: 'http://starrysurrealism.tumblr.com/'
+        ,deviantart:'http://mikymeg.deviantart.com/'
+        ,flickr: 'https://www.flickr.com/photos/fmphotoart/'
+    }
+    ,showTooltips: false
+    ,hoverEffect:''
+    ,pixelsBetweenItems: 20
+    ,theme: 'mikymeg-color'
+    ,size: 64
+    ,itemCss: {'padding-bottom': 0}
+});
+
+$("#themes .theme.mikymeg-grey .sollist").sollist({
+    profiles: {
+        tumblr: 'http://starrysurrealism.tumblr.com/'
+        ,deviantart:'http://mikymeg.deviantart.com/'
+        ,flickr: 'https://www.flickr.com/photos/fmphotoart/'
+    }
+    ,showTooltips: false
+    ,hoverEffect:''
+    ,pixelsBetweenItems: 20
+    ,theme: 'mikymeg-grey'
+    ,size: 64
+    ,itemCss: {'padding-bottom': 0}
+});
+
+$("#themes .theme.fuel .sollist").sollist({
+    profiles: {
+        twitter: 'https://twitter.com/graphicsfuel/'
+        ,dribbble:'https://dribbble.com/GraphicsFuel'
+        ,facebook: 'https://www.facebook.com/graphicsfuel'
+    }
+    ,showTooltips: false
+    ,hoverEffect:''
+    ,pixelsBetweenItems: 20
+    ,theme: 'fuel'
+    ,size: 64
+    ,itemCss: {'padding-bottom': 0}
+});
+
+$("#themes .theme.handdrawn .sollist").sollist({
+    profiles: {
+        instagram: 'https://instagram.com/polinametelkova'
+        ,facebook: 'http://facebook.com/zuck'
+        ,pinterest: 'https://www.pinterest.com/pin/475270566903389502/'
+    }
+    ,showTooltips: false
+    ,hoverEffect:''
+    ,pixelsBetweenItems: 20
+    ,theme: 'handdrawn'
+    ,size: 64
+    ,itemCss: {'padding-bottom': 0}
+});
+
+$("#themes .theme.light-circle .sollist").sollist({
+    profiles: {
+        tumblr: 'http://starrysurrealism.tumblr.com/'
+        ,facebook: 'http://facebook.com/zuck'
+        ,pinterest: 'https://www.pinterest.com/pin/475270566903389502/'
+    }
+    ,showTooltips: false
+    ,hoverEffect:''
+    ,pixelsBetweenItems: 20
+    ,theme: 'light-circle'
+    ,size: 64
+    ,itemCss: {'padding-bottom': 0}
+});
+$("#themes .theme.martz90-hex .sollist").sollist({
+    profiles: {
+        youtube: 'http://www.youtube.com/channel/UCvZ_FcRigdk0gU6Hh8fobjQ'
+        ,deviantart: 'http://martz90.deviantart.com/'
+        ,instagram: 'https://instagram.com/polinametelkova'
+    }
+    ,showTooltips: false
+    ,hoverEffect:''
+    ,pixelsBetweenItems: 20
+    ,theme: 'martz90-hex'
+    ,size: 64
+    ,itemCss: {'padding-bottom': 0}
+});
+$("#themes .theme.victor-bejar .sollist").sollist({
+    profiles: {
+        dribbble: 'https://dribbble.com/victorbejar'
+        ,behance: 'https://www.behance.net/victorbejar'
+        ,linkedin: 'https://www.linkedin.com/in/victorbejar'
+    }
+    ,showTooltips: false
+    ,hoverEffect:''
+    ,pixelsBetweenItems: 20
+    ,theme: 'victor-bejar'
+    ,size: 64
+    ,itemCss: {'padding-bottom': 0}
+});
+
+$("#hover .example.fade-out .sollist").sollist({
+    profiles: {
+        dribbble: 'https://dribbble.com/victorbejar'
+    }
+    ,hoverEffect:'fade-out'
+    ,theme: 'victor-bejar'
+    ,size: 64
+});
+
+$("#hover .example.fade-in .sollist").sollist({
+profiles: {
+        deviantart: 'http://mikymeg.deviantart.com/'
+    }
+    ,hoverEffect:'fade-in'
+    ,theme: 'mikymeg-color'
+    ,size: 64
+});
+
+$("#hover .example.rise .sollist").sollist({
+    profiles: {
+        deviantart: 'http://mikymeg.deviantart.com/'
+    }
+    ,hoverEffect:'rise'
+    ,theme: 'mikymeg-grey'
+    ,size: 64
+});
+
+$("#hover .example.rotate .sollist").sollist({
+    profiles: {
+        twitter: 'https://twitter.com/GarganoLee'
+    }
+    ,hoverEffect:'rotate'
+    ,theme: 'lee-gargano-circle-color'
+    ,size: 64
+});
+
+$("#hover .example.shrink .sollist").sollist({
+    profiles: {
+        behance: 'https://www.behance.net/leegargano'
+    }
+    ,hoverEffect:'shrink'
+    ,theme: 'lee-gargano-square-color'
+    ,size: 64
+});
+
+$("#hover .example.grow .sollist").sollist({
+    profiles: {
+        deviantart: 'http://martz90.deviantart.com/'
+    }
+    ,hoverEffect:'grow'
+    ,theme: 'martz90-hex'
+    ,size: 64
+});
+
+$("#hover .example.bounce .sollist").sollist({
+    profiles: {
+        linkedin: 'http://ca.linkedin.com/in/leegargano/'
+    }
+    ,hoverEffect:'bounce'
+    ,theme: 'lee-gargano-circle-color'
+    ,size: 64
+});
+
+$('#custom-hover').sollist({
+    profiles: {
+        instagram: 'http://instagram.com/polinametelkova'
+    }
+    ,hoverInFunction: function($item){
+        $item.css({"padding-left": "50px", "transition-duration": "0.1s"});
+    }
+    ,hoverOutFunction: function($item){
+        $item.css({"padding-left": "0px", "transition-duration": "0.1s"});
+    }
+});
+
+$(".footer .sollist").sollist({
+    profiles: {
+        github: 'https://github.com/88z'
+    }
+    ,theme: 'lee-gargano-circle-white'
+    ,hoverEffect: 'rotate'
+    ,pixelsBetweenItems: 0
+    ,size: 64
+    ,itemCss: {
+        'padding-bottom' :0
+    }
+});
